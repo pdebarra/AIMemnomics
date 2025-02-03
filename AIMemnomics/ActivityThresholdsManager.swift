f79cdfa777b3d7b882e9af3df4053ce9998cc4e2
Modi MacMod <modi.mac.mod@gmail.com>
2025-02-03 21:01:35 +0000

    Update to progress metrics to expand on what is being presented

---
+++ AIMemnomics/ActivityThresholdsManager.swift
@@ -0,0 +1,8 @@
+//
+//  ActivityThresholdsManager.swift
+//  AIMemnomics
+//
+//  Created by Patrick Barry on 03/02/2025.
+//
+
+import Foundation
---
+++ AIMemnomics/ProgressGraphView.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressGraphView.swift
+//  AIMemnomics
+//
+//  Created by Patrick Barry on 03/02/2025.
+//
+
+import SwiftUI
+
+struct ProgressGraphView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressGraphView()
+}

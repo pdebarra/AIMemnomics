e5028bce4ea47922ccc0322ca696c0534cc4a120
Modi MacMod <modi.mac.mod@gmail.com>
2025-02-03 13:34:35 +0000

    Initial Commit

---
+++ AIMemnomics/AIMemnomicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIMemnomicsApp.swift
+//  AIMemnomics
+//
+//  Created by Patrick Barry on 03/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct AIMemnomicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIMemnomicsTests/AIMemnomicsTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIMemnomicsTests.swift
+//  AIMemnomicsTests
+//
+//  Created by Patrick Barry on 03/02/2025.
+//
+
+import Testing
+@testable import AIMemnomics
+
+struct AIMemnomicsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
